15909ed7b0862771108021106968bd526a5e50e6
senurafdo <tnsenurafernando@gmail.com>
2025-11-23 23:09:32 +0100

    initialize spring boot project

---
+++ src/main/java/com/senura/quality_hub/QualityHubApplication.java
@@ -0,0 +1,13 @@
+package com.senura.quality_hub;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QualityHubApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(QualityHubApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/senura/quality_hub/QualityHubApplicationTests.java
@@ -0,0 +1,13 @@
+package com.senura.quality_hub;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QualityHubApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
